d395e578b0d91ef1b685e0df20a2f4fbb598b9b9
danivdwerf <danivdwerf@gmail.com>
2023-08-30 14:51:49 +0200

    Register the plugin

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,67 +1,78 @@
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
+import {prefixPluginTranslations} from "@strapi/helper-plugin";
 
-import pluginPkg from '../../package.json';
-import pluginId from './pluginId';
-import Initializer from './components/Initializer';
-import PluginIcon from './components/PluginIcon';
+import pluginId from "./pluginId";
+import pluginPkg from "../../package.json";
+import PluginIcon from "./components/PluginIcon";
+import Initializer from "./components/Initializer";
 
 const name = pluginPkg.strapi.name;
 
-export default {
-  register(app: any) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
+export default
+{
+    register(app: any) {
+        app.addMenuLink({
+            to: `/plugins/${pluginId}`,
+            icon: PluginIcon,
+            intlLabel: {
+                id: `${pluginId}.plugin.name`,
+                defaultMessage: name,
+            },
+            Component: async ()=>
+                await import(/* webpackChunkName: "[request]" */ "./pages/App"),
+            
+            permissions: [],
+        });
 
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    });
-    const plugin = {
-      id: pluginId,
-      initializer: Initializer,
-      isReady: false,
-      name,
-    };
+        app.createSettingSection({
+            id: pluginId,
+            intlLabel: {
+                id: `${pluginId}.plugin.name`,
+                defaultMessage: pluginPkg.strapi.name,
+            },
+        }, [{
+            intlLabel: {
+                id: `${pluginId}.plugin.name`,
+                defaultMessage: "Configuration",
+            },
+            id: pluginId,
+            to: `/settings/${pluginId}`,
+            Component: async ()=>
+                await import(/* webpackChunkName: "stripe-page" */ "./pages/Settings"),
+        }]);
 
-    app.registerPlugin(plugin);
-  },
+        app.registerPlugin({
+            name,
+            id: pluginId,
+            isReady: false,
+            initializer: Initializer
+        });
+    },
 
-  bootstrap(app: any) {},
+    bootstrap(app: any)
+    {
 
-  async registerTrads(app: any) {
-    const { locales } = app;
+    },
 
-    const importedTrads = await Promise.all(
-      (locales as any[]).map((locale) => {
-        return import(`./translations/${locale}.json`)
-          .then(({ default: data }) => {
-            return {
-              data: prefixPluginTranslations(data, pluginId),
-              locale,
-            };
-          })
-          .catch(() => {
-            return {
-              data: {},
-              locale,
-            };
-          });
-      })
-    );
+    async registerTrads(app: any)
+    {
+        const {locales} = app;
+        const importedTrads = await Promise.all((locales as any[]).map((locale)=>
+        {
+            return import(`./translations/${locale}.json`)
+                .then(({default: data}) => {
+                    return {
+                        data: prefixPluginTranslations(data, pluginId),
+                        locale,
+                    };
+                })
+                .catch(() => {
+                    return {
+                        data: {},
+                        locale,
+                    };
+                });
+        }));
 
-    return Promise.resolve(importedTrads);
-  },
+        return Promise.resolve(importedTrads);
+    }
 };
